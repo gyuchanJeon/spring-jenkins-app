a7bef8865cfc995198ed8a7fd24cb56b321203aa
gyuchanJeon <ian810900@gmail.com>
2024-10-11 11:25:58 +0900

    second commit

---
+++ src/test/java/co/kr/ianjeon/SpringJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.ianjeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
